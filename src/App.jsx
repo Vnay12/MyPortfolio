8827ca730877cd3b1e64731bd657ec45074e620d
Vnay12 <vnay1712@gmail.com>
2025-05-06 09:22:36 +0530

    My Portfolio

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main>
+        <div>
+            <h1 className='text-3xl font-bold underline text-center mt-10' underline>
+                Welcome to My Portfolio!!!
+            </h1>
+        </div>
+
+    </main>
+  )
+}
+
+export default App
